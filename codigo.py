2caa1b5f3583290b25f2a9f5042c0218626adf36
matheus lopes20 <127550845+Matheustheulopes@users.noreply.github.com>
2025-03-26 20:30:46 -0300

    commit

---
+++ codigo.py
@@ -0,0 +1,11 @@
+# Programa que lê dois números e mostra a soma entre eles
+
+# Solicita ao usuário que insira dois números
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+# Calcula a soma dos números
+s = n1 + n2
+
+# Exibe o resultado
+print(f"A soma entre {n1} e {n2} é {s}. ")
